4bdf5b46acea717a427391647b725387a5b01fdd
looper <chenshuai17@meituan.com>
2024-01-09 20:32:31 +0800

    Initial Commit

---
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -0,0 +1,17 @@
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
+import org.looper.App
+
+fun main() = application {
+    Window(
+        title = "TODO",
+        state = rememberWindowState(width = 800.dp, height = 600.dp),
+        onCloseRequest = ::exitApplication,
+    ) {
+        window.minimumSize = Dimension(350, 600)
+        App()
+    }
+}
